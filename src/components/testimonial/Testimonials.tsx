bf620e443edb4d05b33f9ab2c12664d03c534f09
ApsaraWitharana <asachini095@gmail.com>
2025-01-20 19:22:44 +0530

    add-update

--- src/components/testimonial/Testimonials.tsx
+++ src/components/testimonial/Testimonials.tsx
@@ -1,22 +1,21 @@
 import  'react';
-import Slider from 'react-slick';
 import ava01 from '../../assets/images/ava-1.jpg';
 import ava02 from '../../assets/images/ava-2.jpg';
 import ava03 from '../../assets/images/ava-3.jpg';
 import ava04 from '../../assets/images/ava-4.jpeg';
 import '../../styles/home.css'
 const Testimonials = () => {
-    const sliderSettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
+    // const sliderSettings = {
+    //     // dots: true,
+    //     // infinite: true,
+    //     // speed: 500,
+    //     // slidesToShow: 1,
+    //     // slidesToScroll: 1,
+    // };
 
     return (
         <div className="testimonials-slide d-flex gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 ">
-            <Slider {...sliderSettings}>
+
                 <div className="testimonial-slide d-flex justify-content-around align-items-start gap-4 ">
                     <div className="testimonial py-4 px-3">
                         <p>This service is fantastic! I had an amazing experience and would highly recommend it to
@@ -30,7 +29,7 @@ const Testimonials = () => {
                         </div>
                     </div>
                 </div>
-            </Slider>
+
             <div className="testimonial-slide d-flex justify-content-around align-items-start gap-4 ">
                 <div className="testimonial py-4 px-3">
                     <p>This service is fantastic! I had an amazing experience and would highly recommend it to anyone
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -143,7 +143,7 @@ const Home = () => {
                             <h2 className="testimonial__title">What our fans say about us</h2>
                         </Col>
                         <Col lg="12" >
-                            <Testimonials/>
+                       <Testimonials/>
                         </Col>
                     </Row>
                 </Container>
